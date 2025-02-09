f77668aaf5f4273e4c7230860348d8a7fdb2a02e
herbert-code <herbert.code@gmail.com>
2025-02-09 16:47:48 -0400

    Modelo para Area listo

---
+++ MSFercorp.Venta/Models/Area.cs
@@ -0,0 +1,9 @@
+﻿namespace MSFercorp.Venta.Models
+{
+    public class Area
+    {
+        public int Id { get; set; }
+        public string codigo { get; set; }
+        public string nombre { get; set; }
+    }
+}
